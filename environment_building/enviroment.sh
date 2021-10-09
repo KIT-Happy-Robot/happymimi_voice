53e2171b886e5f74b4ee976a67da89a379ffa510
Happy-fukuda <b1915927@planet.kanazawa-it.ac.jp>
2021-10-09 13:39:37 +0900

    add learned word2vec

--- environment_building/enviroment.sh
+++ environment_building/enviroment.sh
@@ -3,7 +3,7 @@ sudo -E apt install portaudio19-dev
 pip install -r requirements.txt
 DIR=$HOME"/catkin_ws/src/happymimi_voice/config/dataset"
 FILE=$DIR"/stanford-tagger-4.2.0.zip" 
-MG_FILE=$DIR"glove.twitter.27B.200d.magnitude"
+MG_FILE=$DIR"/crawl-300d-2M.magnitude"
 echo $DIR
 if [ ! -e $FILE ]; then
   echo "install stanford data"
@@ -16,12 +16,12 @@ if [ ! -e $FILE ]; then
   fi
 fi
 if [ ! -e $MG_FILE]; then
-    echo "install glove.twitter.27B.200d.magnitude"
-    wget -c http://magnitude.plasticity.ai/glove/heavy/glove.twitter.27B.200d.magnitude
+    echo "install crawl-300d-2M.magnitude"
+    wget -c http://magnitude.plasticity.ai/fasttext/heavy/crawl-300d-2M.magnitude
     if [ ! -d $DIR ]; then
         echo -e "\e[31m ~/catkin_ws/src/happymimi_voice/dataset is not find, p      lease clone happymimi_voice \e[m"
   　else
-        mv glove.twitter.27B.200d.magnitude ../config/dataset/glove-twitter-27B-200d.magnitude
+        mv crawl-300d-2M.magnitude ../config/dataset/
     fi
 fi
 
