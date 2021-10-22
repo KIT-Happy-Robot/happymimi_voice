37fd0cba3beb615eb7403df927b5e01af445a58a
Happy-fukuda <b1915927@planet.kanazawa-it.ac.jp>
2021-10-22 11:38:45 +0900

    ed shfile10/22

--- environment_building/enviroment.sh
+++ environment_building/enviroment.sh
@@ -9,8 +9,8 @@ if [ ! -e $FILE ]; then
   echo "install stanford data"
   wget https://nlp.stanford.edu/software/stanford-tagger-4.2.0.zip
   unzip stanford-tagger-4.2.0.zip
-  if [ ! -d $DIR ]; then
-    echo -e "\e[31m ~/catkin_ws/src/happymimi_voice/dataset is not find, please clone happymimi_voice \e[m"
+  if [ ! -e $DIR ]; then
+    echo "\e[31m ~/catkin_ws/src/happymimi_voice/config/dataset is not find, please clone happymimi_voice \e[m"
   else
     mv stanford-postagger-full-2020-11-17 ~/catkin_ws/src/happymimi_voice/config/dataset/stanford-postagger
   fi
@@ -18,8 +18,8 @@ fi
 if [ ! -e $MG_FILE]; then
     echo "install crawl-300d-2M.magnitude"
     wget -c http://magnitude.plasticity.ai/fasttext/heavy/crawl-300d-2M.magnitude
-    if [ ! -d $DIR ]; then
-        echo -e "\e[31m ~/catkin_ws/src/happymimi_voice/dataset is not find, p      lease clone happymimi_voice \e[m"
+    if [ ! -e $DIR ]; then
+        echo "\e[31m ~/catkin_ws/src/happymimi_voice/config/dataset is not find, p      lease clone happymimi_voice \e[m"
   　else
         mv crawl-300d-2M.magnitude ../config/dataset/
     fi
