965370a5d8ba138e080dd456b7034bd77121c15d
Happy-fukuda <b1915927@planet.kanazawa-it.ac.jp>
2021-10-22 11:47:09 +0900

    ed shfile10/22.2

--- environment_building/enviroment.sh
+++ environment_building/enviroment.sh
@@ -2,27 +2,19 @@
 sudo -E apt install portaudio19-dev
 pip install -r requirements.txt
 DIR=$HOME"/catkin_ws/src/happymimi_voice/config/dataset"
-FILE=$DIR"/stanford-tagger-4.2.0.zip" 
+FILE=$DIR"/stanford-postagger" 
 MG_FILE=$DIR"/crawl-300d-2M.magnitude"
 echo $DIR
 if [ ! -e $FILE ]; then
   echo "install stanford data"
   wget https://nlp.stanford.edu/software/stanford-tagger-4.2.0.zip
   unzip stanford-tagger-4.2.0.zip
-  if [ ! -e $DIR ]; then
-    echo "\e[31m ~/catkin_ws/src/happymimi_voice/config/dataset is not find, please clone happymimi_voice \e[m"
-  else
-    mv stanford-postagger-full-2020-11-17 ~/catkin_ws/src/happymimi_voice/config/dataset/stanford-postagger
-  fi
+  mv stanford-postagger-full-2020-11-17 ../config/dataset/stanford-postagger
 fi
 if [ ! -e $MG_FILE]; then
     echo "install crawl-300d-2M.magnitude"
     wget -c http://magnitude.plasticity.ai/fasttext/heavy/crawl-300d-2M.magnitude
-    if [ ! -e $DIR ]; then
-        echo "\e[31m ~/catkin_ws/src/happymimi_voice/config/dataset is not find, p      lease clone happymimi_voice \e[m"
-  　else
-        mv crawl-300d-2M.magnitude ../config/dataset/
-    fi
+    mv crawl-300d-2M.magnitude ../config/dataset/
 fi
 
 python nltk_download.py
