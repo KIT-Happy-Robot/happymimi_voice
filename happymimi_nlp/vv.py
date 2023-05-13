d899c751adb019526f2508933ceb5113ce8b8375
happykoya <c1103809@planet.kanazawa-it.ac.jp>
2023-05-13 21:58:23 +0900

    ADD:sample

--- happymimi_nlp/vv.py
+++ happymimi_nlp/vv.py
@@ -3,7 +3,9 @@ import voicevox_core
 from voicevox_core import AccelerationMode, AudioQuery, VoicevoxCore
 from playsound import playsound
 
-SPEAKER_ID = 8
+from chat_bot import chat_bot_main
+
+SPEAKER_ID = 3
 
 #　四国めたん   ・ノーマル : 2  ・あまあま : 0  ・ツンツン : 6  ・セクシー : 4
 #  ずんだもん   ・ノーマル : 3  ・あまあま : 1  ・ツンツン : 7  ・セクシー : 5
@@ -16,16 +18,18 @@ SPEAKER_ID = 8
 #  冥鳴ひまり   ・ノーマル : 14
 #  九州そら     ・ノーマル : 16 ・あまあま : 15 ・ツンツン : 18 ・セクシー : 17 ・ささやき : 19
 
-open_jtalk_dict_dir = './open_jtalk_dic_utf_8-1.11'
-text = "who are you"
+open_jtalk_dict_dir = '/home/kouya/Downloads/open_jtalk_dic_utf_8-1.11'
+text = "こんにちは！ハッピーミミです！"
 out = Path('output.wav')
 acceleration_mode = AccelerationMode.AUTO
 
 def main() -> None:
     core = VoicevoxCore(
-        acceleration_mode=acceleration_mode, open_jtalk_dict_dir=open_jtalk_dict_dir
-    )
+        acceleration_mode=acceleration_mode, open_jtalk_dict_dir=open_jtalk_dict_dir)
     core.load_model(SPEAKER_ID)
+    # chat_botとの合わせ技
+    text = chat_bot_main()
+    
     audio_query = core.audio_query(text, SPEAKER_ID)
     wav = core.synthesis(audio_query, SPEAKER_ID)
     out.write_bytes(wav)
