d1adf07cf0cbf39771e73fe5fd35c1e66ed19b83
Koya Okuse <88079640+happykoya@users.noreply.github.com>
2023-05-21 19:47:31 +0900

    Update tts_srvserver.py

--- happymimi_voice_common/src/tts_srvserver.py
+++ happymimi_voice_common/src/tts_srvserver.py
@@ -19,6 +19,12 @@ server = "http://wwwproxy.kanazawa-it.ac.jp:8080"
 os.environ["http_proxy"] = server
 os.environ["https_proxy"] = server
 
+#学外で使用するときはこっち
+server = ""
+os.environ["http_proxy"] = server
+os.environ["https_proxy"] = server
+
+
 class TTS_server(object):
     def __init__(self):
         rospy.init_node('common_texttospeech')
