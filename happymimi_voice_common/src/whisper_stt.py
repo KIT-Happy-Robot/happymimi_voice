d74fc71d2bdb3bad8bc9e3073e4e1f1b1529f0b0
happykoya <c1103809@planet.kanazawa-it.ac.jp>
2023-05-26 17:18:03 +0900

    ADD:whsiper_server

---
+++ happymimi_voice_common/src/whisper_stt.py
@@ -0,0 +1,66 @@
+import whisper
+import pyaudio
+import wave
+import rospy 
+from happymimi_msgs.srv import SetStr, SetStrResponse
+
+class Wshiper_Stt:
+    
+    def __init__(self) -> None:
+        
+        self.FORMAT = pyaudio.paInt16
+        self.wave_filename = "sample.wav"
+        self.CHUNK = 1024
+        self.RECORD_SECOND = 5
+        self.CHANNELS = 1 #モノラル
+        self.RATE = 44100 #サンプルレート（録音の音質）
+        
+        print("wshiper_ready")
+        self.srv = rospy.ServiceProxy("/whisper_stt",SetStr,self.whisper_server)
+        
+    def MakeWavFile(self):
+        
+        p = pyaudio.PyAudio()
+        stream = p.open(format = self.FORMAT,
+                        channels = self.CHANNELS,
+                        rate = self.RATE,
+                        input = True,
+                        frames_per_buffer = self.CHUNK)
+        #レコード開始
+        print("Now Recording...")
+        all = []
+        for i in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECOND)):
+            data = stream.read(self.CHUNK) #音声を読み取って、
+            all.append(data) #データを追加
+        #レコード終了
+        
+        print("Finished Recording.")
+        stream.close()
+        p.terminate()
+        wavFile = wave.open(self.wave_filename, 'wb')
+        wavFile.setnchannels(self.CHANNELS)
+        wavFile.setsampwidth(p.get_sample_size(self.FORMAT))
+        wavFile.setframerate(self.RATE)
+        #wavFile.writeframes(b''.join(all)) #Python2 用
+        wavFile.writeframes(b"".join(all)) #Python3用
+        wavFile.close()
+
+    def whisper_server(self):
+        self.MakeWavFile()
+        #この読み込みを高速化したいかも
+    #実機でのデバッグはdevice="GPU"に変更して 
+        model = whisper.load_model(name="large",device="cpu",in_memory=True)
+        result = model.transcribe(whisper_stt.wave_filename, verbose=False, language="en")
+        print(result["text"]) 
+        
+        return SetStrResponse(result = result["text"])   
+        
+
+if __name__ == "__main__":    
+    rospy.init_node("whisper_stt")
+    whisper_stt = Wshiper_Stt()
+    rospy.spin()
+    
+    #model = whisper.load_model(name="large",device="cpu",in_memory=True)
+    #result = model.transcribe(whisper_stt.wave_filename, verbose=False, language="en")
+    #print(result["text"])
