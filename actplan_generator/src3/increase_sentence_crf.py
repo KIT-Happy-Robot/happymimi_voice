1f350b7db357019526111f47d8ef633d0564536b
happykoya <c1103809@planet.kanazawa-it.ac.jp>
2023-06-21 17:20:33 +0900

    FIX:gitignore

--- actplan_generator/src3/increase_sentence_crf.py
+++ actplan_generator/src3/increase_sentence_crf.py
@@ -23,7 +23,7 @@ MODEL_PATH = "../resource/src3/"
 #word_vec = api.load("glove-twitter-200")
 vectors = Magnitude("/home/kouya/Downloads/crawl-300d-2M.magnitude")
 
-example_sentence1 = "<act>Get</act> the <target>bowl</target> from the <location>chair</location> and <act>put</act> <target>it<target> on the <location>shelf</location>"
+example_sentence1 = "<act>Get</act> the <target>bowl</target> from the <location>chair</location> and <act>put</act> <target>it</target> on the <location>shelf</location>"
 example_sentence2 = "<act>Follow</act> <human>Mary</human>"
 example_sentence3 = "<act>Meet</act> <human>Jennifer</human> at the <location>desk</location>, <act>follow</act> <human>her</human>, and <act>accompany</act> <human>her</human> back" 
 
@@ -33,8 +33,8 @@ class Increase_Sentence():
         self.act_go = ["go","meet","find","contact","face","locate"]
         self.act_navigate = ["navigate","escort","guide","accompany","follow","contact","lead"]
         self.act_tell = ["tell","introduce","speak","say","ask","answer"]
-        self.act_grasp = ["get","grasp","give","bring","put",
-                          "serve","pick","deliver","provide","take"]
+        self.act_give = ["give","put","serve","provide"]
+        self.act_grasp = ["get","grasp","bring","pick","deliver","take"]
 
         self.location = ["guest-room","kitchen","bed room","living-room","cab",
                          "toilet","operator","desk","shelf","chair","entrance","table"]
@@ -66,7 +66,7 @@ class Increase_Sentence():
         self.posdict = {}
 
     def compare_similarity_action(self,word):
-        word_list = ["go","navigate","tell","grasp"]
+        word_list = ["go","navigate","tell","give","grasp"]
         word_dict = {}
         result = []
         max_value = []
@@ -74,7 +74,7 @@ class Increase_Sentence():
             result.append(vectors.similarity(word,word_list[i]))
             word_dict[word_list[i]] = result[i]
             
-        #print(word_dict)
+        print(word,word_dict)
         max_key = max(word_dict.items(), key = lambda x:x[1])
         return max_key[0]
         
@@ -88,7 +88,7 @@ class Increase_Sentence():
             result.append(vectors.similarity(word,word_list[i]))
             word_dict[word_list[i]] = result[i]
             
-        #print(word_dict)
+        print(word,word_dict)
         max_key = max(word_dict.items(), key = lambda x:x[1])
         return max_key[0]
 
@@ -198,7 +198,7 @@ class Increase_Sentence():
 
             return buffer, posdict
         
-    def random_execute_fix(self, root):
+    def random_generate_fix(self, root):
             buffer = ""
             pos = 0
             num_act = 1
@@ -211,14 +211,18 @@ class Increase_Sentence():
             
             for sentence in root:
                 if sentence.tag == "act":
-                    if self.compare_similarity_action(sentence.text) == 'go': 
+                    result = self.compare_similarity_action(sentence.text)
+                    if result == 'go': 
                         act = random.choice(self.act_go)
-                    elif self.compare_similarity_action(sentence.text) == 'navigate':
+                    elif result == 'navigate':
                         act = random.choice(self.act_navigate)
-                    elif self.compare_similarity_action(sentence.text) == 'tell':
+                    elif result == 'tell':
                         act = random.choice(self.act_tell)
-                    elif self.compare_similarity_action(sentence.text) == 'grasp':
+                    elif result == 'grasp':
                         act = random.choice(self.act_grasp)
+                    elif result == 'give':
+                        act = random.choice(self.act_give)    
+                    
 
                     buffer += act
                     if "act_0" in posdict:
@@ -242,13 +246,18 @@ class Increase_Sentence():
 
                                         
                 elif sentence.tag == "target":
-                    if self.compare_similarity_target(sentence.text) == 'object':
-                        target = random.choice(self.tar_object)
-                    elif self.compare_similarity_target(sentence.text) == 'day':
-                        target = random.choice(self.tar_do)
-                    elif self.compare_similarity_target(sentence.text) == 'human':
-                        target = random.choice(self.tar_human)
+                    #result = self.compare_similarity_target(sentence.text) 
+                    if result == 'object':
+                        #target = random.choice(self.tar_object)
+                        pass
+                    elif result == 'day':
+                        #target = random.choice(self.tar_do)
+                        pass
+                    elif result == 'human':
+                        #target = random.choice(self.tar_human)
+                        pass
                     
+                    target = sentence.text
                     buffer += target
                     if "target_0" in posdict:
                         posdict["target_"+ str(num_tar)] = (pos,pos+len(target))
@@ -259,12 +268,15 @@ class Increase_Sentence():
                     pos += len(target)
 
                 elif sentence.tag == "human":
-                    human = random.choice(self.human)
+                    if sentence.text not in self.human_other:
+                        human = random.choice(self.human)
+                    else:
+                        human = sentence.text
+                            
                     buffer += human
                     if "human_0" in posdict:
                         posdict["human_"+ str(num_hum)] = (pos,pos+len(human))
                         num_hum += 1
-                        
                     else:
                         posdict["human_0"] = (pos,pos+len(human))
                     pos += len(human)
@@ -327,40 +339,55 @@ class Increase_Sentence():
         print("finish increase")
         f.close()
         
-        def sample_execute(self):
-            line1 = example_sentence1
-            line2 = example_sentence2
-            line3 = example_sentence3
+    def sample_execute(self):
+        line1 = example_sentence1
+        line2 = example_sentence2
+        line3 = example_sentence3
+        
+        
+        #print(line1)
+        
+        root1 = xml.etree.ElementTree.fromstring("<dummy>"+line1+"</dummy>")
+        root2 = xml.etree.ElementTree.fromstring("<dummy>"+line2+"</dummy>")
+        root3 = xml.etree.ElementTree.fromstring("<dummy>"+line3+"</dummy>")
+        
+        for i in range(1):
+            sen1, posdict = self.random_generate_fix(root1)
+            sen2, posdict = self.random_generate_fix(root2)
+            sen3, posdict = self.random_generate_fix(root3)
             
-            root = xml.etree.ElementTree.fromstring("<dummy>"+line1+"</dummy>")
+            print("sen1_before:",example_sentence1)
+            print("sen2_before:",example_sentence2)
+            print("sen3_before:",example_sentence3)
+            print("-----------------------------------")    
+            print("sen1:",sen1)
+            print("sen2:",sen2)
+            print("sen3:",sen3)
             
-            for i in range(1):
-                sen, posdict = self.random_generate(root)
-                print(sen)
-                lis = []
-                pos = 0
-                prev_label = 0
-                '''
-                line = self.split_and_pos(sen)
-                #print("posdict:",posdict)
-                for j in range(len(line)):
-                    
-                    word = line[j][0]
-                    postag = line[j][1]
-                    label = self.get_label(pos, posdict)
-                    #print("pos:",pos,"word:",word)
-                    if label == "O":
-                        lis.append([word,postag,"O"])
-                    elif label == prev_label:
-                        lis.append([word,postag,"I-"+label])
-                    else:
-                        lis.append([word,postag,"B-"+label])
-                                
-                    pos += len(word)
-                    pos += 1
-                    prev_label = label
-                '''
+            lis = []
+            pos = 0
+            prev_label = 0
+            '''
+            line = self.split_and_pos(sen)
+            #print("posdict:",posdict)
+            for j in range(len(line)):
+                
+                word = line[j][0]
+                postag = line[j][1]
+                label = self.get_label(pos, posdict)
+                #print("pos:",pos,"word:",word)
+                if label == "O":
+                    lis.append([word,postag,"O"])
+                elif label == prev_label:
+                    lis.append([word,postag,"I-"+label])
+                else:
+                    lis.append([word,postag,"B-"+label])
+                            
+                pos += len(word)
+                pos += 1
+                prev_label = label
+            '''
         
 if __name__ == "__main__":
     incse = Increase_Sentence()
-    incse.execute()
+    incse.sample_execute()
